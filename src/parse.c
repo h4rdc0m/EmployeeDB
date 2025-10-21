274b1af3c5d0b7a867c82c07289631c647807760
Christiaan van Dam <chris@trace.nl>
2025-10-21 08:48:05 +0000

    Corrigeer foutieve controle op malloc in validate_db_header en create_db_header

--- src/parse.c
+++ src/parse.c
@@ -46,7 +46,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
     }
 
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == -1) {
+    if (header == NULL) {
         printf("Malloc failed to create a db header\n");
         return STATUS_ERROR;
     }
@@ -89,7 +89,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
-    if (header == -1) {
+    if (header == NULL) {
         printf("Malloc failed to create db header\n");
         return STATUS_ERROR;
     }
