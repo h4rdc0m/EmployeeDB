2caf5887aabe099d03df93b999ff7afc4403d9dd
Christiaan van Dam <chris@trace.nl>
2025-10-21 08:50:32 +0000

    Verwijder foutcontrole voor ongeldige file descriptor in create_db_header

--- src/parse.c
+++ src/parse.c
@@ -87,12 +87,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
     return STATUS_SUCCESS;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
-    if (fd < 0) {
-        printf("Got bad FD from the user\n");
-        return STATUS_ERROR;
-    }
-    
+int create_db_header(struct dbheader_t **headerOut) {
 	struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
     if (header == NULL) {
         printf("Malloc failed to create db header\n");
